6e2cdbf5cf63e85c126f3b0c988f97e0ed2adb63
OMGDuke <lex1989@gmail.com>
2016-07-26 13:36:00 +0100

    Refactored machine to es6 class

--- src/machine.js
+++ src/machine.js
@@ -1,39 +1,41 @@
-function Machine(slots) {
-  this._totalBalance = 100;
-  this.readyToPlay = false;
-  this.slots = slots;
+class Machine {
+  constructor() {
+    this._totalBalance = 100;
+    this.readyToPlay = false;
+    this.slots = slots;
+  }
 
-  this._payout = function(player) {
-    player.receiveWinnings(this._totalBalance);
-    this._totalBalance = 0;
-  };
-
-  Machine.prototype.currentBalance = function () {
+  currentBalance() {
     return this._totalBalance;
-  };
+  }
 
-  Machine.prototype.addCredit = function (player) {
+  addCredit(player) {
     if(this.readyToPlay) { throw new Error("Machine is already in credit"); }
     player.insertCoin();
     this._totalBalance += 3;
     this.readyToPlay = true;
-  };
+  }
 
-  Machine.prototype.pullLever = function (player) {
+  pullLever(player) {
     if(!this.readyToPlay) { throw new Error("Please insert £3 to play"); }
     this.readyToPlay = false;
     var slotResult = this.slots.spin();
-    var outcome = _calculateOutcome(slotResult);
-    return outcome === true ? _win(player, slotResult) : _lose(slotResult);
-  };
+    var outcome = this._calculateOutcome(slotResult);
+    return outcome === true ? this._win(player, slotResult) : this._lose(slotResult);
+  }
 
-  function _calculateOutcome(result) {
+  _calculateOutcome(result) {
     return !!result.reduce(function(a, b){ return (a === b) ? a : NaN; });
   }
 
-  function _win(player, slotResult) {
+  _win(player, slotResult) {
     var winnings = this._totalBalance;
     this._payOut(player);
     return 'You Win! Result: ' + slotResult + ". Winnings: £" + winnings;
+  }
+
+  _payOut(player) {
+    player.receiveWinnings(this._totalBalance);
+    this._totalBalance = 0;
   }
 }
